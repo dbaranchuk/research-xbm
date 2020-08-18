defd2bc34a75baf25e5048445c341a9f3112ab92
Dmitry Baranchuk <dmitrybaranchuk@gmail.com>
2020-08-18 11:53:14 +0300

    .

--- ret_benchmark/config/model_path.py
+++ ret_benchmark/config/model_path.py
@@ -14,9 +14,9 @@ from yacs.config import CfgNode as CN
 
 MODEL_PATH = dict()
 MODEL_PATH = {
-    "bninception": "~/backbones/bn_inception-52deb4733.pth",
-    "resnet50": "~/backbones/pytorch_resnet50.pth",
-    "googlenet": "~/backbones/googlenet-1378be20.pth",
+    "bninception": "./backbones/bn_inception-52deb4733.pth",
+    "resnet50": "./backbones/pytorch_resnet50.pth",
+    "googlenet": "./backbones/googlenet-1378be20.pth",
 }
 
 MODEL_PATH = CN(MODEL_PATH)
