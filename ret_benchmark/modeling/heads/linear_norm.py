fb198e1d9d44337d1d1b4fa45eb09402a64bd071
Dmitry Baranchuk <dmitrybaranchuk@gmail.com>
2020-09-05 11:37:50 +0300

    .

--- ret_benchmark/modeling/heads/linear_norm.py
+++ ret_benchmark/modeling/heads/linear_norm.py
@@ -9,16 +9,18 @@ from ret_benchmark.utils.init_methods import weights_init_kaiming
 class LinearNorm(nn.Module):
     def __init__(self, cfg):
         super(LinearNorm, self).__init__()
+        self.bn1 = nn.BatchNorm1d(cfg.MODEL.HEAD.IN_CHANNELS)
         self.fc1 = nn.Linear(cfg.MODEL.HEAD.IN_CHANNELS, cfg.MODEL.HEAD.IN_CHANNELS)
         self.fc1.apply(weights_init_kaiming)
 
-        self.bn = nn.BatchNorm1d(cfg.MODEL.HEAD.IN_CHANNELS)
+        self.bn2 = nn.BatchNorm1d(cfg.MODEL.HEAD.IN_CHANNELS)
         self.fc2 = nn.Linear(cfg.MODEL.HEAD.IN_CHANNELS, cfg.MODEL.HEAD.DIM)
         self.fc2.apply(weights_init_kaiming)
 
     def forward(self, x):
+        x = self.bn1(x)
         x = self.fc1(x)
-        x = self.bn(x)
+        x = self.bn2(x)
         x = self.fc2(x)
         x = nn.functional.normalize(x, p=2, dim=1)
         return x
